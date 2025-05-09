1cd628c45ee4a0ec57eece64d4924a2e275606e4
Richuan_Li <rc620706@dal.ca>
2025-05-09 01:28:12 -0300

    [Cognizant_A_2]-Removed unused files

--- Exceptions/A1_Exception.py
+++
@@ -1,6 +0,0 @@
-class OperationNotFoundError(Exception):
-    """ Exception raised when an operation is not found.
-    """
-    def __init__(self, message, input_value):
-        super().__init__(message)
-        self.input_value = input_value
